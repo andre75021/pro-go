fa96c33fc51d26c3bbae1c7252ca359360ef468a
Andre Luiz <andrelluiz.rodrigues@gmail.com>
2023-05-27 19:19:38 -0300

    finish loadTemplate function

--- partyinvites/main.go
+++ partyinvites/main.go
@@ -1,8 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"text/template"
+)
+
+type Rsvp struct {
+	Name, Email, Phone string
+	WillAttend         bool
+}
+
+var responses = make([]*Rsvp, 0, 10)
+var templates = make(map[string]*template.Template, 3)
+
+// loadTemplate() Load a template from an array
+func loadTemplate() {
+	templateNames := [5]string{"welcome", "form", "thanks", "sorry", "list"}
+	for index, name := range templateNames {
+		t, err := template.ParseFiles("layout.html", name+".html")
+		if err == nil {
+			templates[name] = t
+			fmt.Println("Loaded template", index, name)
+		} else {
+			panic(err)
+		}
+	}
+}
 
 func main() {
-	fmt.Println("ToDo: add some feature")
-	fmt.Println("ToDo: add some feature2")
+	loadTemplate()
 }
